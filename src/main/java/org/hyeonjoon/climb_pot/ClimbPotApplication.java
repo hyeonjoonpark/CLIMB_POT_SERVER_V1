1179526f7910cd0f7361d3a3e406486cd2faff99
hyeonjoonpark <pjjoon1379@gmail.com>
2025-03-20 15:59:08 +0900

    SET : project setting

---
+++ src/main/java/org/hyeonjoon/climb_pot/ClimbPotApplication.java
@@ -0,0 +1,13 @@
+package org.hyeonjoon.climb_pot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbPotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClimbPotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hyeonjoon/climb_pot/ClimbPotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hyeonjoon.climb_pot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbPotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
