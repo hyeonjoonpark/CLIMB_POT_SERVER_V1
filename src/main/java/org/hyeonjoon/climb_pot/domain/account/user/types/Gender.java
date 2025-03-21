29d56f747ec9a7e634124064fa661e5ad3c87f7f
hyeonjoonpark <pjjoon1379@gmail.com>
2025-03-21 14:25:30 +0900

    FIX : Enum

--- src/main/java/org/hyeonjoon/climb_pot/domain/account/user/types/Gender.java
+++ src/main/java/org/hyeonjoon/climb_pot/domain/account/user/types/Gender.java
@@ -1,14 +1,6 @@
 package org.hyeonjoon.climb_pot.domain.account.user.types;
 
-import lombok.Getter;
-
-@Getter
 public enum Gender {
-    MALE("남자"),
-    FEMALE("여자");
-    private final String value;
-
-    Gender(String value) {
-        this.value = value;
-    }
+    MALE,
+    FEMALE
 }
--- src/main/java/org/hyeonjoon/climb_pot/domain/account/user/types/Role.java
+++ src/main/java/org/hyeonjoon/climb_pot/domain/account/user/types/Role.java
@@ -1,14 +1,6 @@
 package org.hyeonjoon.climb_pot.domain.account.user.types;
 
-import lombok.Getter;
-
-@Getter
 public enum Role {
-    ROLE_ADMIN("ROLE_ADMIN"),
-    ROLE_USER("ROLE_USER");
-    private final String name;
-
-    Role(String name) {
-        this.name = name;
-    }
+    ROLE_ADMIN,
+    ROLE_USER
 }
